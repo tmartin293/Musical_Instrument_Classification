f3e6329442f91bc422f5939dce3a584279a4982f
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2019-05-03 14:54:15 -0700

    Update startup.py

--- startup.py
+++ startup.py
@@ -36,12 +36,7 @@ Teardown
 
 
 
-"""
-Based on the tutorial from Adafruit: 
-https://github.com/adafruit/Adafruit_Python_CharLCD/blob/master/examples/char_lcd.py
-"""
-import Adafruit_CharLCD as LCD
-import Adafruit_BBIO.GPIO as GPIO
+import Output # LCD and LED
 from scipy.io.wavfile import read
 from scipy.fftpack import dct, idct
 from numpy import empty_like
@@ -54,62 +49,7 @@ import wave
 import sys
 import time
 
-"""
-LCD configuration
-"""
-lcd_rs        = 'P8_8'
-lcd_en        = 'P8_10'
-lcd_d4        = 'P8_18'
-lcd_d5        = 'P8_16'
-lcd_d6        = 'P8_14'
-lcd_d7        = 'P8_12'
-lcd_backlight = 'P8_7'
 
-# Define LCD column and row size for 16x2 LCD.
-lcd_columns = 16
-lcd_rows    = 2
-
-# Initialize the LCD using the pins above.
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                           lcd_columns, lcd_rows, lcd_backlight)
-
-def toggle_cursor(state):
-    lcd_clear()
-    # state is a Boolean, example state = True to turn it on
-    lcd.show_cursor(state)
-
-# Max String
-max_disp    =   "1234567890123456\n1234567890123456"
-"""
-LCD Error Strings
-"""
-# Audio Streaming
-mic_err     =   "Failed to\nConnect to Mic\n"
-record_err  =   "Failed to\nSave Audio\n"
-len_err     =   "Recording Length\nExceeded\n"
-# Preprocessing
-mfcc_err    =   "Failed to\nPreprocess Audio\n"
-# ML
-predict_err =   "Failed to Guess\nInstrument\n"
-"""
-LCD Instrument Strings
-"""
-# Correct Output
-results     =   "Instrument =\n"
-instruments = [ "Cello\n", "Clarinet\n", "Flute\n",
-                "Guitar\n", "Saxophone\n", "Trumpet\n",
-                "Violin\n"]
-def lcd_print(msg):
-    lcd.message(msg)
-
-def lcd_clear():
-    lcd.clear()
-
-def print_inst(num):
-    if(num < len(instruments) and num > len(instruments)):
-        lcd_print("Wrong Instrument\nDetected\n")
-    else:
-        lcd_print(results + instruments[num])
 
 """
 Button Configuration
