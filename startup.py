c39956a3ed552370a05b8708410f81bbb77f3792
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2019-05-06 10:39:33 -0700

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,12 +1,21 @@
+import Input
+import Output
+import Prediction
+
 """
 Configuration
 """
-# LCD Setup display - 'Loading...'
-    # Import Pickle Model
-    # Setup Compenents
-        # Button
-        # LED
-        # Mic
+# Setup Compenents
+lcd = Output.BBB_LCD()
+button = Input.Button()
+led = Output.LED()
+mic = Input.Mic()
+
+# Display Setup
+# stretch goal: add a cool graphic loading message
+lcd.print("Loading...")
+# Import Pickle Model
+
 
 """
 Event Loop
@@ -30,60 +39,3 @@ Teardown
 """
 # GPIO Cleanup
 # Audio Stream Terminate
-
-
-
-
-
-
-import Output # LCD and LED
-from scipy.io.wavfile import read
-from scipy.fftpack import dct, idct
-from numpy import empty_like
-import numpy as np
-import pyaudio
-import datetime
-import pickle
-import math
-import wave
-import sys
-import time
-
-
-
-"""
-Button Configuration
-"""
-button = "P8_8"
-
-def SetupButton():
-    GPIO.setup(button,GPIO.IN)
-    GPIO.add_event_detect(button,GPIO.RISING)
-
-def CleanAll():
-    GPIO.cleanup()
-
-def EventDetected():
-    if(GPIO.event_detected(button)):
-        return True
-    return False
-	
-"""
-This should be were we include all the parts of the software
-I am going to wait until Friday to finish this
-"""
-
-
-def main():
-    timeout = 100
-    counter = 0
-    lcd_print("Press Button\nTo Record\n")
-    while(counter < timeout):
-        time.sleep(0.1)
-        if(EventDetected()):
-            print("Button pressed!")
-        counter = counter + 1
-    CleanAll()
-        
-
-main()
