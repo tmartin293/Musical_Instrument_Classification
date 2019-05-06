1a74d1fd14cf7dba5b7d6efaab8bbca687092985
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2019-05-06 11:35:07 -0700

    Update ConfigStartup.py

--- ConfigStartup.py
+++ ConfigStartup.py
@@ -1,10 +1,24 @@
 import os
 
+def ConfigCondaEnvVars(filepath):
+    os.chdir("$CONDA_PREFIX")
+    # Setup the activate file
+    os.system("mkdir -p ./etc/conda/activate.d")
+    os.system("touch ./etc/conda/activate.d/env_vars.sh")
+    env_var = open("./etc/conda/activate.d/env_vars.sh")
+    env_var.write("#!/bin/sh\nexport MY_FILE=%s", filepath)
+    # Setup the deactivate file
+    os.system("mkdir -p ./etc/conda/deactivate.d")
+    os.system("touch ./etc/conda/deactivate.d/env_vars.sh")
+    env_var = open("./etc/conda/activate.d/env_vars.sh")
+    env_var.write("#!/bin/sh\nunset %s", filepath)
+
 def MakeStartup(filename):
+    ConfigCondaEnvVars( ("/usr/bin/"+filename) )
     os.chdir("/usr/bin/")
     f = open("startup.sh","w+")
     f.write("#!/bin/bash\n"+
-        "/home/%s" % filename)
+        "%s/etc/conda/activate.d/%s" % filename)
     f.close()
     os.system("chmod u+x /usr/bin/%s", filename)
     MakeService(filename)
@@ -20,7 +34,7 @@ def MakeService(filename):
         "WantedBy=multi-user.target", filename)
     f.close()
     os.chdir("/etc/systemd/system/")
-    os.system("ln /lib/systemd/%s.service scriptname.service", filename)
+    os.system("ln /lib/systemd/%s.service %s.service", filename, filename)
     SetConfig(filename)
     
 def SetConfig(filename):
