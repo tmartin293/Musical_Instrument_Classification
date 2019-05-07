90a4b05eafa7f6f7c63ee52bba53a04c8ed411f1
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2019-05-07 13:15:25 -0700

    Updated Input.py code

--- Input.py
+++ Input.py
@@ -1,5 +1,6 @@
 
 import Adafruit_BBIO.GPIO as GPIO
+import Output
 import pyaudio
 import datetime
 import wave
@@ -22,48 +23,54 @@ class Button:
         enable edge detection using add_event_detect() first. Pin should be 
         type IN.". Since the function call returns a Boolean, no need for if-else
         """
-        return(GPIO.event_detected(self.button))
+        return GPIO.event_detected(self.button)
 
     def cleanup(self):
         GPIO.cleanup()
 
 
 class Mic:
-    def __init__(self):
+    def __init__(self, lcd):
         self.mic_str = "AmazonBasics Portable USB Mic"
+        self.audio = pyaudio.PyAudio()
+        self.device_index = self.mic_setup()
+        if(self.device_index == -1):
+            lcd.print("Unable to connect to microphone.")
+            self.audio.terminate()
+        self.sample_rate = 44100
+        self.audio_format = pyaudio.paInt16
+        self.audio_channel = 1
+        self.chunk = 8192
+        self.frames = []
+        self.stream = None
+        self.counter = 0
 
-    def mic_setup(self,audio):
-        # setup microphone
-        for i in range(audio.get_device_count()):
-            test_str = audio.get_device_info_by_index(i).get('name')
-            if(test_str.startswith(self.mic_str)):
-                return(i)
-        return(-1)
+    def mic_setup(self):
+        for i in range(self.audio.get_device_count()):
+            test_str = self.audio.get_device_info_by_index(i).get('name')
+            if test_str.startswith(self.mic_str):
+                return i
+        return -1
 
+    # Instead of returning the stream, initialize the object's variable to the stream
+    def start_stream(self,audio_format = pyaudio.paInt16):
+        self.stream = self.audio.open(format = self.audio_format,rate = self.sample_rate, \
+                          channels = self.audio_channel,input_device_index = self.device_index, \
+                          input = True, frames_per_buffer = self.chunk)
 
-    def start_stream(self,audio,device_index,sample_rate = 44100,audio_format = pyaudio.paInt16,\
-                     audio_channel = 1,chunk = 8192):
-
-        return(audio.open(format = audio_format,rate = sample_rate, \
-                          channels = audio_channel,input_device_index = device_index, \
-                          input = True,frames_per_buffer = chunk))
-
-
-    def save_audio(self,frames,audio,stream,audio_channel = 1,sample_rate = 44100,\
-                   audio_format = pyaudio.paInt16):
-        stream.stop_stream()
-        stream.close()
-        now = datetime.datetime.now()
-        file_name = "audio_test_" + str(now.month) + "-" + str(now.day) + "-" + \
-                    str(now.year) + "__" + str(now.hour) + "-" + str(now.minute) + \
-                    "-" + str(now.second) + "_.wav"
+    # Do we really need to return the filename? I think a new system should be implemented which
+    # deals with audio files in some way
+    def save_audio(self, audio_format = pyaudio.paInt16):
+        self.stream.stop_stream()
+        self.stream.close()
+        file_name = "audio_test_" + datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S") + "_.wav"
         wavefile = wave.open(file_name,'wb')
-        wavefile.setnchannels(audio_channel)
-        wavefile.setsampwidth(audio.get_sample_size(audio_format))
-        wavefile.setframerate(sample_rate)
-        wavefile.writeframes(b''.join(frames))
+        wavefile.setnchannels(self.audio_channel)
+        wavefile.setsampwidth(self.audio.get_sample_size(audio_format))
+        wavefile.setframerate(self.sample_rate)
+        wavefile.writeframes(b''.join(self.frames))
         wavefile.close()
-        return(file_name)
+        return file_name 
 
-    def cleanup(self,audio):
-        audio.terminate()
+    def cleanup(self):
+        self.audio.terminate()
--- Prediction.py
+++ Prediction.py
@@ -6,16 +6,10 @@ import numpy as np
 
 class Predict:
     def __init__(self):
-        self.model = None
+        self.model = open("pickle_model.pkl", 'rb')
 
-    def import_ml_model(self, model_name = "pickle_model.pkl"):
-        with open(model_name, 'rb') as file:
-            self.model = pickle.load(file)
-
-
+    # Dictionary of possibe instrument predictions
     def instrument_prediction(self, prediction):
-        
-        # Dictionary of possibe instrument predictions
         switch = {
             1: "Cello",
             2: "Clarinet",
@@ -25,57 +19,47 @@ class Predict:
             6: "Trumpet",
             7: "Violin",
         }
-        
         # Return instrument or N/A if the input is not in the dictionary
-        return(switch.get(prediction,"N/A"))
-
+        return switch.get(prediction,"N/A")
 
     def get_predictions(self,filename,model,sample_rate = 44100,top_db_limit = 35,\
                         hop = int(44100/100),n_fft = int(44100/50),mfcc_size = 14):
         
         data,sample_rate = librosa.load(filename,sr=sample_rate)
         data,index = librosa.effects.trim(data,top_db=top_db_limit)
-        onset_frames = librosa.onset.onset_detect(y=data,sr=sample_rate,\
-                                                hop_length=hop)
+        onset_frames = librosa.onset.onset_detect(y=data,sr=sample_rate, hop_length=hop)
         notes = hop * onset_frames
         predictions = []
-
         # Predict instrument for each detected note
-		for i in range(0,len(notes)):
-		
-    		if(i < len(notes)-1):
-        		note_data = data[notes[i]:notes[i+1]]
-    		else:
-        		note_data = data[notes[i]:-1]
+        for i in range(0, len(notes)):
+            if i < len(notes)-1:
+                note_data = data[notes[i]:notes[i+1]]
+            else:
+                note_data = data[notes[i]:-1]
+                
+            mfcc = librosa.feature.mfcc(note_data,sr=sample_rate, n_fft=n_fft,hop_length=hop, n_mfcc=mfcc_size)
+            mfcc = np.mean(mfcc[1:,:],axis=1)
+            mfcc = np.reshape(mfcc,(1,-1))
+            result = model.predict(mfcc)[0]
+            predictions.append(result)
 
-    		mfcc = librosa.feature.mfcc(note_data,sr=sample_rate,\
-                                        n_fft=n_fft,hop_length=hop,\
-                                        n_mfcc=mfcc_size)
-
-    		mfcc = np.mean(mfcc[1:,:],axis=1)
-    		mfcc = np.reshape(mfcc,(1,-1))
-    		result = model.predict(mfcc)[0]
-    		predictions.append(result)
-
-    	# Determine prediction accuracy for each instruement detected
-    	result_strs = []
-    	unique,counts = np.unique(predictions,return_counts=True)
-    	index = np.argsort(counts)
-    	index = np.flip(index)
-    	unique = unique[index]
-    	counts = counts[index]
+            # Determine prediction accuracy for each instruement detected
+            result_strs = []
+            unique,counts = np.unique(predictions,return_counts=True)
+            index = np.argsort(counts)
+            index = np.flip(index)
+            unique = unique[index]
+            counts = counts[index]
        
-    	for i in range(0,len(unique)):
-        	accuracy = (counts[i] / len(predictions)) * 100
-        	prediction_str = instrument_prediction(unique[i]) + \
-                             "\n{:3.3f}".format(accuracy) + "%\n"
-        	"""
-        	print(instrument_prediction(unique[i]),"\n"\
-              "{:3.3f}".format(accuracy),"%\n")
-        	"""
-        	result_strs.append(prediction_str)
-
-    	return(result_strs)
+        for i in range(0,len(unique)):
+            accuracy = (counts[i] / len(predictions)) * 100
+            prediction_str = self.instrument_prediction(unique[i]) + "\n{:3.3f}".format(accuracy) + "%\n"
+            """
+            print(instrument_prediction(unique[i]),"\n"\
+                "{:3.3f}".format(accuracy),"%\n")
+            """
+            result_strs.append(prediction_str)
+        return(result_strs)
 
 
 
